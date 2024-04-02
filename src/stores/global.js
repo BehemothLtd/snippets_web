5da1198a9f6178f0ce37598e00d4da5520bf4a0c
behemoth-cuongdv <cuongdv@behemoth.vn>
2024-04-02 09:56:21 +0700

    refactor axios loading

--- src/stores/global.js
+++ src/stores/global.js
@@ -4,10 +4,12 @@ import { defineStore } from "pinia";
 import { get } from "lodash";
 
 import axios from "@/apis/axios";
+import gqlRequest from "@/apis/gqlClient";
 
 export const useGlobalStore = defineStore("global", () => {
   const validationErrors = ref({});
   const errorMessage = ref("");
+  const requests = ref([]);
 
   function getErrors(name) {
     return get(validationErrors.value, name, []);
@@ -21,17 +23,28 @@ export const useGlobalStore = defineStore("global", () => {
     validationErrors.value = value;
   }
 
+  function removeRequest(payload) {
+    setTimeout(() => {
+      requests.value = requests.value.filter((item) => item.id !== payload.id);
+    }, 300);
+  }
+
+  function addRequest(payload) {
+    requests.value.push(payload);
+  }
+
   async function uploadImages(formData) {
-    try {
-      const { data } = await axios.post("/uploads", formData);
-      return data.data;
-    } catch (error) {
-      alert(error);
-    }
+    const response = await gqlRequest(
+      null,
+      { data: formData },
+      { loading: true, toast: false, requestType: "upload" }
+    );
+    return response;
   }
 
   return {
     // Errors Msgs
+    requests,
     errorMessage,
     validationErrors,
 
@@ -39,5 +52,7 @@ export const useGlobalStore = defineStore("global", () => {
     setValidationErrors,
     getErrors,
     setErrorMessage,
+    addRequest,
+    removeRequest,
   };
 });
